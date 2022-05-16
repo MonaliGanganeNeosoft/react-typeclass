f7b11204bcf2ed40fabe2ba768a94c4513931083
MoniNeo <moni.gangane@gmail.com>
2022-05-16 14:44:22 +0530

    interface of child and parent components

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,40 @@
+//Interfaces
+interface Profile {
+  readonly name: string;
+  age?: number;
+}
+let profile: Profile = {
+  name: "Moni",
+};
+//index Signature
 interface A {
-  someProp: number;
+  someProp: string;
+  [key: string]: number | string;
 }
-interface B {
-  someProp: number;
-  anotherProp: number;
+const a: A = { someProp: "Some Prop" };
+a.x = 1;
+a.y = 2;
+
+//Call Signature
+interface Sum {
+  (a: number, b: number): number;
+  prop1: string;
 }
-let a: A = { someProp: 1 };
-let b: B = { someProp: 7, anotherProp: 8 };
-console.log(b);
+const sum: Sum = (a, b) => a + b;
+sum.prop1 = "Some Prop";
+console.log(sum.prop1);
+console.log(sum(2, 8));
+
+//Extending interfaces
+interface Parent {
+  x: string;
+}
+interface Parent2 {
+  y: string;
+}
+interface Parent3 {
+  z: string;
+}
+interface Child extends Parent, Parent2, Parent3 {}
+let Child: Child = { x: "Some prop", y: "y prop", z: "z prop" };
+console.log(Child);
